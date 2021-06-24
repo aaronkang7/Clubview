0622e5c342aa5b71596bd4ad78be28b0aa1bce5c
Aaron Kang <aaronkang612@gmail.com>
2021-06-24 01:46:12 -0700

    delete functionality

---
+++ client/src/components/Toast/Toast.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { createPortal } from "react-dom";
+import { check } from "../../icons/other/index";
+import styles from "./confirmationtoast.module.css";
+
+const Toast = ({ message }) => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {visible &&
+        createPortal(
+          <div className="toast">
+            <p className="toasttext">{message}</p>
+          </div>,
+          document.body
+        )}
+    </>
+  );
+};
+
+export default Toast;
