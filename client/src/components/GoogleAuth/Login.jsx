1e2bf4766d7aa6a041fe348dc8c0d62f505d902a
Aaron Kang <aaronkang612@gmail.com>
2021-06-27 02:59:44 -0700

    .:D

--- client/src/components/GoogleAuth/Login.jsx
+++ client/src/components/GoogleAuth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import axios from "axios";
 import { refreshTokenSetup } from "../../utils/refreshToken";
 import { AuthContext, UserContext } from "../../context/user";
