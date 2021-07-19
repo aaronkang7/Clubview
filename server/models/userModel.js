e9c098fe5a2e13db70c9e4acb1428bff8c9863f6
Aaron Kang <aaronkang612@gmail.com>
2021-07-19 01:24:20 -0700

    passport.js setup

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose";
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+export const userSchema = new Schema({
   firstName: String,
   lastName: String,
   imageUrl: String,
   email: String,
   favorites: [{ type: Schema.Types.ObjectId, ref: "Club" }],
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,45 +1,7 @@
 import express from "express";
 import passport from "passport";
 import cookieSession from "cookie-session";
-import "../passport.js";
 
 const router = express.Router();
-
-const isLoggedIn = (req, res, next) => {
-  if (req.user) {
-    next();
-  } else {
-    res.sendStatus(401);
-  }
-};
-
-router.use(passport.initialize());
-router.use(passport.session());
-
-router.use(
-  cookieSession({
-    name: "clubView-session",
-    keys: ["key1", "key2"],
-  })
-);
-
-router.get("/", (req, res) => res.send("You are now logged in"));
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
-
-router.get("/failed", (req, res) => {
-  res.send("You have failed to login");
-});
-
-router.get("/success", isLoggedIn, (req, res) =>
-  res.send(`Welcome ms ${req.user}`)
-);
-
-router.get("/");
-
-router.get("/logout", (req, res) => {
-  req.session = null;
-  req.logout();
-  req.redirect("/");
-});
 
 export default router;
