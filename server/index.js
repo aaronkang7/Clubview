8d0b68f979a3c358d84be1c422592d69b7eec18a
Aaron Kang <aaronkang612@gmail.com>
2021-07-19 01:24:04 -0700

    passport.js setup

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import passport from "passport";
+import session from "express-session";
 import clubRoutes from "./routes/clubs.js";
 import profileRoutes from "./routes/user.js";
 import authRoutes from "./routes/auth.js";
+import User from "./models/userModel.js";
 
 const app = express();
 dotenv.config();
@@ -16,12 +19,22 @@ app.use("/clubs", clubRoutes);
 app.use("/profile", profileRoutes);
 app.use("/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello ClubView API");
-});
-
 const PORT = process.env.PORT || 5000;
 
+//Session setup
+
+app.use(
+  session({
+    secret: "Our little secret...",
+    resave: false,
+    saveUninitialized: false, //CHANGE LATER
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//MongoDB Section
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
@@ -33,3 +46,13 @@ mongoose
   .catch((error) => console.log(error));
 
 mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+//Passort Section
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", (req, res) => {
+  res.send("Hello ClubView API");
+});
